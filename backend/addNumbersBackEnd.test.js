d8a41c95398462b9ee9b84a452511933aba77639
Michael Steward <michael.steward@biorender.com>
2021-07-13 16:53:13 -0700

    Add test for multiply

--- backend/addNumbersBackEnd.test.js
+++ backend/addNumbersBackEnd.test.js
@@ -1,5 +1,8 @@
-const { addNumbers } = require('./addNumbersBackEnd');
+const { addNumbers, multiplyNumbers } = require('./addNumbersBackEnd');
 
 test('adds numbers', () => {
   expect(addNumbers(1,1)).toBe(2);
+})
+test('multiply numbers', () => {
+  expect(multiplyNumbers(2,2)).toBe(4);
 })
